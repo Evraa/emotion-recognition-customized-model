a2571bf0b2cd83dcf95ed14cfe59bce7f0fcb1ce
Evraa <evramyousef@gmail.com>
2021-02-07 03:20:51 +0200

    added data_loaders

--- consts.py
+++ consts.py
@@ -2,5 +2,5 @@ _dataset_path = 'data/fer2013/fer2013.csv'
 _dataset_png_path = './data/data_png'
 _image_dim = 48
 _emotions = {0: 'angry', 3:'happy', 4:'sad', 6:'neutral' }
-_num_classes = 4
 _epochs = 0
+_batch_size = 32
--- train.py
+++ train.py
@@ -12,15 +12,15 @@ from facenet_pytorch import MTCNN
 import torch.nn.functional as F
 import time
 #local imports
-from consts import _num_classes, _epochs
+from consts import _epochs, _dataset_png_path,_batch_size
 
 # Implementing the model
-def model_init():
+def model_init(num_classes):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     #read the model RESNET18
     model = models.resnet18(pretrained=True)
     num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, _num_classes)
+    model.fc = nn.Linear(num_features, num_classes)
     model = model.to(device) #if cuda or nots
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -28,8 +28,28 @@ def model_init():
     return model, criterion, exp_lr_scheduler
 
 
+def data_load():
+    # Creating the train/test dataloaders from images
+    root_data_dir = _dataset_png_path
+    #transform the data
+    transform = transforms.Compose([transforms.RandomResizedCrop(224),transforms.RandomHorizontalFlip(),transforms.ToTensor(),transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    total_dataset = datasets.ImageFolder(root_data_dir, transform)
+    #split train and test
+    train_size = int(0.8 * len(total_dataset))
+    test_size = len(total_dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(total_dataset, [train_size, test_size])
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, shuffle=True, num_workers=4)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=_batch_size, shuffle=True, num_workers=4)
+
+    class_names = total_dataset.classes
+    num_classes = len(class_names)
+    return train_dataloader, test_dataloader, num_classes
+
+
 def run():
-    model, criterion, exp_lr_scheduler = model_init()
+    train_dataloader, test_dataloader, num_classes = data_load()
+    model, criterion, exp_lr_scheduler = model_init(num_classes)
     for epoch in range(_epochs):
         print(f'=== EPOCH {epoch} / {_epochs} ===')
         train()
